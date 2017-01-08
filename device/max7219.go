226f7eb6206184e7c73a9b454e955d5bd4836475
Sebastian Florek <sebastian.florek@ts.fujitsu.com>
2017-01-08 18:12:55 +0100

    Poll temperature from server and display

--- device/max7219.go
+++ device/max7219.go
@@ -1,23 +1,115 @@
 package device
 
 import (
-	"github.com/fulr/spidev"
 	"fmt"
+
+	"github.com/fulr/spidev"
 )
+
+type Max7219Reg byte
+
+const (
+	MAX7219_REG_NOOP   Max7219Reg = 0
+	MAX7219_REG_DIGIT0            = iota
+	MAX7219_REG_DIGIT1
+	MAX7219_REG_DIGIT2
+	MAX7219_REG_DIGIT3
+	MAX7219_REG_DIGIT4
+	MAX7219_REG_DIGIT5
+	MAX7219_REG_DIGIT6
+	MAX7219_REG_DIGIT7
+	MAX7219_REG_DECODEMODE
+	MAX7219_REG_INTENSITY
+	MAX7219_REG_SCANLIMIT
+	MAX7219_REG_SHUTDOWN
+	MAX7219_REG_DISPLAYTEST = 0x0F
+	MAX7219_REG_LASTDIGIT   = MAX7219_REG_DIGIT7
+)
+
+const MAX7219_DIGIT_COUNT = MAX7219_REG_LASTDIGIT - MAX7219_REG_DIGIT0 + 1
 
 // Max7219 - represents 2 cascaded 8x8 devices. Code is written specially for this device
 // https://botland.com.pl/led-do-raspberry-pi-32/4509-matryca-128-led-16x8-max7219-do-raspberry-pi.html
 // may not work correctly with other configurations.
 type Max7219 struct {
-	buffer []byte
 	device *spidev.SPIDevice
+}
+
+// Init device and Max7219 driver
+func (this Max7219) Init() {
+	this.Command(MAX7219_REG_SCANLIMIT, 7)   // show all 8 digits
+	this.Command(MAX7219_REG_DECODEMODE, 0)  // use matrix (not digits)
+	this.Command(MAX7219_REG_DISPLAYTEST, 0) // no display test
+	this.Command(MAX7219_REG_SHUTDOWN, 1)    // not shutdown mode
+	this.Command(MAX7219_REG_INTENSITY, 7)   // Set brightness (0-15)
+}
+
+func (this Max7219) Command(reg Max7219Reg, value byte) error {
+	buf := []byte{byte(reg), value}
+
+	_, err := this.device.Xfer(buf)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (this Max7219) send(reg Max7219Reg, values ...byte) error {
+	buf := make([]byte, 0)
+	for _, value := range values {
+		buf = append(buf, byte(reg)+1)
+		buf = append(buf, value)
+	}
+
+	_, err := this.device.Xfer(buf)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (this Max7219) ClearDisplay() error {
+	for i := 0; i < MAX7219_DIGIT_COUNT; i++ {
+		err := this.send(Max7219Reg(i), 0, 0)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// TODO add doc
+func (this Max7219) DisplayTemperature(temperature float32) error {
+	// Round to integer number as we don't have space for floating point numbers
+	temp := int(temperature)
+
+	buf, err := DEFAULT_FONT.FromInt(temp)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < MAX7219_DIGIT_COUNT; i++ {
+		err := this.send(Max7219Reg(i), buf[1][i], buf[0][i])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (this Max7219) DisplayAlcoholConcentration(perMilles float32) {
+
 }
 
 func (this Max7219) Close() {
 	this.device.Close()
 }
 
-func NewMax7219() (*spidev.SPIDevice, error) {
+func NewMax7219() (*Max7219, error) {
 	devstr := fmt.Sprintf("/dev/spidev%d.%d", 0, 0)
 
 	spi, err := spidev.NewSPIDevice(devstr)
@@ -25,5 +117,5 @@ func NewMax7219() (*spidev.SPIDevice, error) {
 		return nil, err
 	}
 
-	return spi, nil
+	return &Max7219{device: spi}, nil
 }
--- main.go
+++ main.go
@@ -1,144 +1,70 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/floreks/led-max7219-client/device"
-	"github.com/fulr/spidev"
+
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-type Max7219Reg byte
-
-var MAX7219_CLEAR_DISPLAY = []byte{0, 0, 0, 0, 0, 0, 0, 0}
-
-const (
-	MAX7219_REG_NOOP   Max7219Reg = 0
-	MAX7219_REG_DIGIT0            = iota
-	MAX7219_REG_DIGIT1
-	MAX7219_REG_DIGIT2
-	MAX7219_REG_DIGIT3
-	MAX7219_REG_DIGIT4
-	MAX7219_REG_DIGIT5
-	MAX7219_REG_DIGIT6
-	MAX7219_REG_DIGIT7
-	MAX7219_REG_DECODEMODE
-	MAX7219_REG_INTENSITY
-	MAX7219_REG_SCANLIMIT
-	MAX7219_REG_SHUTDOWN
-	MAX7219_REG_DISPLAYTEST = 0x0F
-	MAX7219_REG_LASTDIGIT   = MAX7219_REG_DIGIT7
-)
-
-const MAX7219_DIGIT_COUNT = MAX7219_REG_LASTDIGIT - MAX7219_REG_DIGIT0 + 1
-
-func send(reg Max7219Reg, value byte, device *spidev.SPIDevice) error {
-	buf := []byte{byte(reg), value}
-
-	_, err := device.Xfer(buf)
-	if err != nil {
-		return err
-	}
-
-	return nil
+type TempResponse struct {
+	Temperature float32 `json:"temperature"`
 }
 
-func sendMulti(reg Max7219Reg, value, value2 byte, device *spidev.SPIDevice) error {
-	buf := []byte{byte(reg), value, byte(reg), value2}
+const tempServerUrl = "http://192.168.0.100:30024/ds18b20"
+const POLLING_TIME = 5 * time.Second
 
-	_, err := device.Xfer(buf)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func sendBufferLine(position int, buffer []byte, device *spidev.SPIDevice) error {
-	reg := MAX7219_REG_DIGIT0 + position
-	fmt.Printf("Register: %#x\n", reg)
-	buf := make([]byte, 2)
-	for i := 0; i < 1; i++ {
-		b := buffer[i*MAX7219_DIGIT_COUNT+position]
-		fmt.Printf("Buffer value: %#x\n", b)
-		buf[i*2] = byte(reg)
-		buf[i*2+1] = b
-	}
-	fmt.Printf("Send to bus: %v\n", buf)
-	_, err := device.Xfer(buf)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func flush(device *spidev.SPIDevice, buffer []byte) error {
-	for i := 0; i < MAX7219_DIGIT_COUNT; i++ {
-		err := sendBufferLine(i, buffer, device)
+func pollTemperature(tempChan chan float32) {
+	for {
+		tempResponse := new(TempResponse)
+		r, err := http.Get(tempServerUrl)
 		if err != nil {
-			return err
+			log.Printf("Error durring temperature polling: %s", err.Error())
 		}
+
+		json.NewDecoder(r.Body).Decode(tempResponse)
+		tempChan <- tempResponse.Temperature
+		time.Sleep(POLLING_TIME)
+		r.Body.Close()
 	}
-	return nil
 }
 
-func clearDisplay(device *spidev.SPIDevice) error {
-	err := flush(device, MAX7219_CLEAR_DISPLAY)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func displayTemp(first, second int, spi *spidev.SPIDevice) {
-	for i := 0; i < 8; i++ {
-		sendMulti(Max7219Reg(MAX7219_REG_DIGIT0+i), DEFAULT_FONT[first][i], DEFAULT_FONT[second][i], spi)
-	}
+func registerSigtermHandler(dev *device.Max7219) {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		dev.ClearDisplay()
+		os.Exit(1)
+	}()
 }
 
 func main() {
-
-
-
-	// Setup
-	devstr := fmt.Sprintf("/dev/spidev%d.%d", 0, 0)
-	spi, err := spidev.NewSPIDevice(devstr)
+	dev, err := device.NewMax7219()
 	if err != nil {
-		fmt.Printf("Error during device init: %s", err.Error())
+		fmt.Printf("Error during device creation: %s", err.Error())
 		return
 	}
 
-	defer spi.Close()
+	defer dev.Close()
 
-	// Init driver
-	send(MAX7219_REG_SCANLIMIT, 7, spi)   // show all 8 digits
-	send(MAX7219_REG_DECODEMODE, 0, spi)  // use matrix (not digits)
-	send(MAX7219_REG_DISPLAYTEST, 0, spi) // no display test
-	send(MAX7219_REG_SHUTDOWN, 1, spi)    // not shutdown mode
-	send(MAX7219_REG_INTENSITY, 7, spi)   // Set brightness (0-15)
+	tempChan := make(chan float32)
+	go pollTemperature(tempChan)
 
-	clearDisplay(spi)
+	registerSigtermHandler(dev)
 
-	displayTemp(5, 2, spi)
+	dev.ClearDisplay()
+	for temp := range tempChan {
+		dev.DisplayTemperature(temp)
+	}
+
+	for _, b := range device.DEFAULT_FONT[0] {
+		fmt.Printf("%b\n", b)
+	}
 }
-
-//const POLLING_TIME = 2 * time.Second
-//
-//func pollTemperature(tempChan chan float32) {
-//	for {
-//		// Switch to poll from server
-//		randTemp := rand.Float32() * 30.0
-//
-//		tempChan <- randTemp
-//		time.Sleep(POLLING_TIME)
-//	}
-//}
-//
-//func main() {
-//	tempChan := make(chan float32)
-//	go pollTemperature(tempChan)
-//
-//	for temp := range tempChan {
-//		fmt.Printf("Current temperature: %d\n", int32(temp))
-//	}
-//}
